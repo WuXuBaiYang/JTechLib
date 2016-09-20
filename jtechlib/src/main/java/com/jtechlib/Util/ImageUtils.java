c08e4d0ee2cfbe189d9f8f0febdc9305a6998e0c
姜函 <wuxubaiyang@gmail.com>
2016-09-20 15:48:34 +0800

    改了一下图片下载方法

--- jtechlib/src/main/java/com/jtechlib/Util/ImageUtils.java
+++ jtechlib/src/main/java/com/jtechlib/Util/ImageUtils.java
@@ -2,14 +2,26 @@ package com.jtechlib.Util;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 /**
  * 图片通用类
@@ -72,6 +84,40 @@ public class ImageUtils {
                 .error(errorResId)
                 .placeholder(placeholderResId)
                 .into(imageView);
+    }
+
+    /**
+     * 请求图片，回调bitmap
+     *
+     * @param context
+     * @param uri
+     */
+    public static void requestImage(final Context context, String uri, Action1<? super Bitmap> action) {
+        Observable.just(uri)
+                .subscribeOn(Schedulers.io())
+                .map(new Func1<String, Bitmap>() {
+                    @Override
+                    public Bitmap call(String uri) {
+                        if (!TextUtils.isEmpty(uri)) {
+                            try {
+                                File file = Glide.with(context)
+                                        .load(uri)
+                                        .downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                                        .get();
+                                if (null != file && !TextUtils.isEmpty(file.getAbsolutePath())) {
+                                    return BitmapFactory.decodeFile(file.getAbsolutePath());
+                                }
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            } catch (ExecutionException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        return null;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(action);
     }
 
     /**
