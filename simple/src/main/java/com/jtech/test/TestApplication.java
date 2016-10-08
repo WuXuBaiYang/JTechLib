d84befb294f40539b5a04bfac011f8f1f452582b
姜函 <wuxubaiyang@gmail.com>
2016-10-08 09:55:38 +0800

    app改为simple

---
+++ simple/src/main/java/com/jtech/test/TestApplication.java
@@ -0,0 +1,11 @@
+package com.jtech.test;
+
+import com.jtechlib.BaseApplication;
+
+/**
+ * 测试实现方法
+ * Created by jianghan on 2016/9/20.
+ */
+public class TestApplication extends BaseApplication {
+
+}
