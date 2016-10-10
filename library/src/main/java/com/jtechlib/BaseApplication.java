cba2c076b9d8eefb3af1c6569c50c048e908317a
姜函 <wuxubaiyang@gmail.com>
2016-10-10 15:00:50 +0800

    库更新，去掉application中的instance方法

--- library/src/main/java/com/jtechlib/BaseApplication.java
+++ library/src/main/java/com/jtechlib/BaseApplication.java
@@ -10,23 +10,10 @@ import com.karumi.dexter.Dexter;
  */
 public abstract class BaseApplication extends Application {
 
-    private static BaseApplication INSTANCE;
-
     @Override
     public void onCreate() {
         super.onCreate();
-        //赋值instance
-        this.INSTANCE = this;
         //初始化权限检查库
         Dexter.initialize(getApplicationContext());
-    }
-
-    /**
-     * 获取实例
-     *
-     * @return
-     */
-    public static BaseApplication getInstance() {
-        return INSTANCE;
     }
 }
